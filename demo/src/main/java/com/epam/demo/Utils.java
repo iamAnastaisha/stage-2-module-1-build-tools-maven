91df73197b8c3bc1b49f6722f13ac034bbb76b30
Nastya Garkavaya <anastaishastil2002@gmail.com>
2023-03-05 23:50:53 +0300

    commit 0

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+       for (String str : args) {
+           if (Integer.parseInt(str)  <= 0)
+               return true;
+       }
+       return false;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,19 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        int num;
+        double number;
+        try {
+            num = Integer.parseInt(str);
+            return num >= 0;
+        } catch (NumberFormatException ex) {
+            try {
+                number = Double.parseDouble(str);
+                return number >= 0.;
+            } catch (NumberFormatException ex1) {
+                System.out.println("Wrong data!");
+            }
+        }
+        return false;
     }
 }
